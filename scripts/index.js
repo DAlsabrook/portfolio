9339f4bf754ddc88a0f7ee834c01672fd83f8596
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-02 00:10:48 -0500

    big push for new hero logos

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    document.querySelector('.heroTextWrapper').classList.add('reveal');
+  }, 1000); // Adjust delay as needed
+});
+
+
+/**
+ * HTML
+ * CSS
+ * JS
+ * Bootstrap
+ * Figma
+ *
+ * Flask
+ * Python
+ * C
+ * React
+ * Next
+ * Github
+ * MySQL
+ * Firebase
+ * JQuery
+ * Docker
+ */
