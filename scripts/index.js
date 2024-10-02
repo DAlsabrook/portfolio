4e490cc0032352a3b743beaf0cfeeeabd4dff830
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-02 02:26:24 -0500

    Update to header. Added tech icons and animations

--- scripts/index.js
+++ scripts/index.js
@@ -3,23 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.heroTextWrapper').classList.add('reveal');
   }, 1000); // Adjust delay as needed
 });
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    const topLogos = document.querySelectorAll('.heroLogosTop div');
+    const bottomLogos = document.querySelectorAll('.heroLogosBottom div');
 
-
-/**
- * HTML
- * CSS
- * JS
- * Bootstrap
- * Figma
- *
- * Flask
- * Python
- * C
- * React
- * Next
- * Github
- * MySQL
- * Firebase
- * JQuery
- * Docker
- */
+    topLogos.forEach(logo => logo.classList.add('animate'));
+    bottomLogos.forEach(logo => logo.classList.add('animate'));
+  }, 3000); // 1 second delay
+});
