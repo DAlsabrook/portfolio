04b8a9ca113cf38088c4897bb9ceaf63cab07779
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-10-25 11:30:54 -0500

    env variable for cloudflare token

--- scripts/githubGraph.js
+++ scripts/githubGraph.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   async function getGitHubContributions() {
     try {
-      const octokit = new Octokit({ auth: `` });
+      const octokit = new Octokit({ auth: env.githubToken });
       const userContDiv = document.getElementById('user-contribution');
       const today = new Date().toISOString();
       const lastYear = new Date(new Date().setFullYear(new Date().getFullYear() - 1)).toISOString();
